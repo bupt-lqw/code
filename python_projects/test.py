7ba393005f2f8cb3fdae8d822a7f349b374f5947
lqw <qiangweili520@gmail.com>
2025-05-12 20:29:35 +0800

    add mps env

--- python_projects/test.py
+++ python_projects/test.py
@@ -4,3 +4,15 @@ for i in range(10):
 
 
 print(1 * 1)
+
+import torch
+print(torch.__version__)
+device = torch.device("mps")
+
+a = torch.tensor([1, 2])
+print(a.device)
+
+
+a = torch.tensor([1, 2]).to(device)
+print(a.device)
+
